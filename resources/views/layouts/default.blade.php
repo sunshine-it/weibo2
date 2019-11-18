c097c860bd321c350e409606066793e333f5f6e8
Allen <pazmnew@163.com>
2019-11-18 12:52:33 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo2 App')</title>
-    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
