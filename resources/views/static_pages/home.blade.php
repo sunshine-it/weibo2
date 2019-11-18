874617c71f6c8a190a8ccba09e0b6187f868287b
Allen <pazmnew@163.com>
2019-11-18 12:35:54 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel6+</h1>
+    <p class="lead">
+      你现在所学习的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel6+ 复习教程</a> 的示例项目主页。
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+  </div>
 @stop
