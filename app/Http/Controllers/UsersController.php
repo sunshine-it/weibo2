2e4a7fac2ef4cec47b1575831aa341b4a0b95eff
Allen <pazmnew@163.com>
2019-11-24 02:48:43 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,11 +8,18 @@ use App\Models\User;
 // 用户控制器类
 class UsersController extends Controller
 {
+    // 创建用户
     public function create() {
         return view('users.create');
     }
-
+    // 用户列表
     public function show(User $user) {
         return view('users.show', compact('user'));
+    }
+    // 保存用户
+    public function store(Request $request) {
+        // 验证
+        $this->validate($request, ['name'=>'required|max:50', 'email'=>'required|email|unique:users|max:255', 'password'=>'required|confirmed|min:6']);
+        return;
     }
 }
