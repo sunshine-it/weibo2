520cf1af112d95e8d3fe40f431041948558d27f3
Allen <pazmnew@163.com>
2019-11-24 07:10:09 +0000

    fix

--- app/helpers.php
+++ app/helpers.php
@@ -1,20 +1,22 @@
 <?php
 
 if (!function_exists('get_db_config')) {
-    function get_db_config() {
+    function get_db_config()
+    {
         if (getenv('IS_IN_HEROKU')) {
+            $url = parse_url(getenv("DATABASE_URL"));
             return $db_config = [
-                'connection'=> 'pgsql',
-                'host'      => $url["host"],
+                'connection' => 'pgsql',
+                'host' => $url["host"],
                 'database'  => substr($url["path"], 1),
                 'username'  => $url["user"],
                 'password'  => $url["pass"],
             ];
         }
-        else{
+        else {
             return $db_config = [
-                'connection'=> env('DB_CONNECTION', 'mysql'),
-                'host'      => env('DB_HOST', 'localhost'),
+                'connection' => env('DB_CONNECTION', 'mysql'),
+                'host' => env('DB_HOST', 'localhost'),
                 'database'  => env('DB_DATABASE', 'forge'),
                 'username'  => env('DB_USERNAME', 'forge'),
                 'password'  => env('DB_PASSWORD', ''),
