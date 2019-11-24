061a7d0efed04301ad3afb02ef4f95fda8a79cde
Allen <pazmnew@163.com>
2019-11-24 07:45:25 +0000

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request; // 使用 Request 实例来接收用户的所有输入数据
+use Auth;
+
+// 会话控制器类 用于处理用户登录退出相关的操作
+class SessionsController extends Controller
+{
+    // 显示登录页面
+    public function create(){
+        return view('sessions.create');
+    }
+    // 创建新会话（登录）
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+        if (Auth::attempt($credentials)) {
+            // 登录成功后的相关操作
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        }
+        else {
+            // 登录失败后的相关操作
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+        return;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,7 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 // 用户资源路由
 Route::resource('users', 'UsersController');
-
+// 会话路由
+Route::get('login', 'SessionsController@create')->name('login'); // 显示登录页面
+Route::post('login', 'SessionsController@store')->name('login'); // 创建新会话（登录）
+Route::delete('logout', 'SessionsController@destroy')->name('logout'); // 销毁会话（退出登录）
