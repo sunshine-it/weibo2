c96b802472233786e4d04f82ca4e6572d2612539
Allen <pazmnew@163.com>
2019-12-14 04:36:32 +0000

    社交统计信息

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+// 用于注册种子类
 class DatabaseSeeder extends Seeder
 {
     public function run()
@@ -12,7 +13,8 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         // 指定调用微博数据填充文件
         $this->call(StatusesTableSeeder::class);
-
+        // 指定调用用户关注粉丝数据填充文件
+        $this->call(FollowersTableSeeder::class);
         Model::reguard();
     }
 }
